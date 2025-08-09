934625aa8f42c4693e06995479094da6d2a67d09
Shafqat Ali Gujjar <sa10181922@gmail>
2025-08-09 10:33:59 +0500

    Before code.

---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Passing Plain & Custom Data Among Screen In Jetpack Compose"
+include(":app")
+ 
